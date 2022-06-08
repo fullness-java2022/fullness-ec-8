0de0617271fb52bc201b44496a91ffb24dcf49e7
Ryosuke Takahashi <r-takahashi@image-pit.com>
2022-06-08 23:45:39 +0900

    first commit

---
+++ src/main/java/com/fullness/ec/FullnessEc1Application.java
@@ -0,0 +1,13 @@
+package com.fullness.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullnessEc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullnessEc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullness/ec/FullnessEc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullness.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullnessEc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
